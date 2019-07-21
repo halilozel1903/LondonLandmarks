1dd4932e657c7c9d32150753c4c5927f1d7485ce
halilozel1903@gmail.com <halilozel1903@gmail.com>
2019-07-21 17:34:40 +0300

    Project detail page started

---
+++ LondonLandmarks/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  LondonLandmarks
+//
+//  Created by Halil Özel on 21.07.2019.
+//  Copyright © 2019 Halil Özel. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class DetailViewController: UIViewController {
+    
+    
+    @IBOutlet weak var detailImage: UIImageView!
+    @IBOutlet weak var detailTitle: UILabel!
+    @IBOutlet weak var detailDescription: UILabel!
+    @IBOutlet weak var detailTextView: UITextView!
+    @IBOutlet weak var detailMapView: MKMapView!
+    
+    var sentData1 : String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        self.navigationItem.title = sentData1
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func directions(_ sender: Any) {
+        
+    }
+    
+}
--- LondonLandmarks/TableViewController.swift
+++ LondonLandmarks/TableViewController.swift
@@ -90,14 +90,25 @@ class TableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        
+        if(segue.identifier == "showDetail"){
+            
+            let dvc = segue.destination as! DetailViewController
+            
+            if let indexPath = self.tableView.indexPathForSelectedRow{
+                
+                let item = self.items[indexPath.row]
+                
+                dvc.sentData1 = item["Name"]
+            }
+        }
+        
     }
-    */
+ 
 
 }
